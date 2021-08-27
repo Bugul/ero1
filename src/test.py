b80ad62e9f5107cf5991d4306113da74b60512d3
Victor PELLAN <victor.pellan@epita.fr>
2021-08-27 17:30:06 +0200

    Ajout fonction duplication plus court chemin entre deux noeuds

--- src/test.py
+++ src/test.py
@@ -1,7 +1,6 @@
 import osmnx as ox
-from networkx import MultiDiGraph, MultiGraph, generate_edgelist
-
-print("hello")
+import networkx as nx
+from networkx import MultiDiGraph, MultiGraph
 
 place = "Semerville, France"
 digraph: MultiDiGraph = ox.graph_from_place(place, network_type='drive')
@@ -14,8 +13,16 @@ def odd_v(g):
     return node_list
 
 
-print(odd_v(G_projected))
+def complete_path_two_nodes(g, vertex1, vertex2):
+    shortest = nx.dijkstra_path(g, vertex1, vertex2)
+    for i in range(len(shortest) - 1):
+        edge = g.get_edge_data(shortest[i], shortest[i + 1])[0]
+        g.add_edge(shortest[i], shortest[i + 1], length=edge["length"])
 
 
-
-
+print("Liste des sommets de degré impair : ", odd_v(G_projected))
+print("Plus court chemin entre noeud 1812939356 et 1613798843 : ", nx.dijkstra_path(graph, 1812939356, 1613798843))
+print("Liste des routes avant duplication : ", graph.edges.data("length"))
+print("Duplication du plus court chemin entre noeud 1812939356 et 1613798843...")
+complete_path_two_nodes(graph, 1812939356, 1613798843)
+print("Liste des routes après duplication : ", graph.edges.data("length"))
