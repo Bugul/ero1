e6193c7de65e20348f043cf91632244528b1bb44
Bastien HOORELBEKE <bh.69@live.fr>
2021-08-27 15:49:17 +0200

    Test de Semerville

--- src/test.py
+++ src/test.py
@@ -1,10 +1,21 @@
 import osmnx as ox
-from networkx import MultiDiGraph, MultiGraph
+from networkx import MultiDiGraph, MultiGraph, generate_edgelist
 
 print("hello")
 
-place = "Montréal, Canada"
+place = "Semerville, France"
 digraph: MultiDiGraph = ox.graph_from_place(place, network_type='drive')
 graph: MultiGraph = digraph.to_undirected()
 G_projected = ox.project_graph(graph)
-ox.plot_graph(G_projected)
+
+
+def odd_v(g):
+    node_list = [vertex for vertex, deg in g.degree() if deg % 2 == 1]
+    return node_list
+
+
+print(odd_v(G_projected))
+
+
+
+
